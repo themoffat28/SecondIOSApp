570946c4565b2ba1a21f660ed2cf1ddd3e27e810
themoffat28 <bthemoffat@gmail.com>
2016-09-26 08:55:04 -0600

    Put GUI and added instructions in code.

--- SecondIOSApp/ViewController.swift
+++ SecondIOSApp/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var firstTextLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func clickButton(sender:
+        UIButton)
+    {
+        firstTextLabel.text = "Ow, that hurt!"
     }
 
 
