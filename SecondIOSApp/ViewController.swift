3124f943d6c55f56c4ce5de121af7cbe085e5bad
themoffat28 <bthemoffat@gmail.com>
2016-09-26 09:51:55 -0600

    Created random color generator function.

--- SecondIOSApp/ViewController.swift
+++ SecondIOSApp/ViewController.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class ViewController: UIViewController
 {
-
+    @IBOutlet var backround: UIView!
     @IBOutlet weak var firstButton: UIButton!
-    
     @IBOutlet weak var firstTextLabel: UILabel!
     
     override func viewDidLoad()
@@ -21,7 +20,7 @@ class ViewController: UIViewController
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning()
+ override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,8 +29,20 @@ class ViewController: UIViewController
         UIButton)
     {
         firstTextLabel.text = "Ow, that hurt!"
+        firstTextLabel.textColor = makeRandomColor()
+        backround.backgroundColor = makeRandomColor()
     }
-
+        private func makeRandomColor() -> UIColor
+        {
+            let randomColor : UIColor
+            let red :CGFloat = CGFloat(drand48())
+            let green :CGFloat = CGFloat(drand48())
+            let blue :CGFloat = CGFloat(drand48())
+            
+            randomColor = UIColor( red: red, green: green, blue: blue, alpha: 1.0)
+            
+            return randomColor
+    }
 
 }
 
